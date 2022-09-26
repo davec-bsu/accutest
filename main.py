fe682c2f27c137b57e77e1a7b4d8c9db1183a5e8
Dave <github@aukware.com>
2022-09-26 19:39:55 -0400

    This is my first commit, here are my basic files.

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import configparser
+
+
+def get_apikey():
+    config = configparser.ConfigParser()
+    config.read('app.config')
+    apikey_from_file = config['secrets']['apikey']
+    print(apikey_from_file)
+    return apikey_from_file
+
+
+class NoSuchLocation(Exception):
+    pass
+
+
+def get_location():
+    location_url = 'https://dataservice.accuweather.com/locations/v1/' \
+                'postalcodes/search?apikey=APIKEYGOESHERE&q=02324'
+
+    response = requests.get(location_url)
+
+    try:
+        key = response.json()[0].get('Key')
+    except IndexError:
+        raise NoSuchLocation()
+    return key
+
+
+def get_conditions(key):
+    conditions_url = 'https://dataservice.accuweather.com/currentconditions/v1/' \
+        '{}?apikey=APIKEYGOESHERE'.format(key)
+    response = requests.get(conditions_url)
+    json_version = response.json()
+    print("Current Conditions: {}".format(json_version[0].get('WeatherText')))
+
+
+try:
+    apikey = get_apikey()
+    location_key = get_location()
+    get_conditions(location_key)
+except NoSuchLocation:
+    print("Unable to get the location")
